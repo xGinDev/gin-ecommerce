6881b6391d54dda153516f7397a17d65bda9b17b
xGinDev <jecl2912@gmail.com>
2023-06-19 12:21:24 -0500

    fix: prueba netfily

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
 
   return (
     <div>
-      <h1>Hi</h1>
+      <h1 className='title'>Hi</h1>
       {titles.map(title => <p key={title}>{title}</p>)}
     </div>
   );
